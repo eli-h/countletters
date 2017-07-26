0598cacb06b5dc62d9f796d8c1182853dcd9473e
eli-h <eli.heineman@gmail.com>
2017-07-26 17:06:49 +0000

    solved

--- countletters.js
+++ countletters.js
@@ -1,3 +1,14 @@
-function countletters() {
-	
+function countLetters(string) {
+	var string2 = string.replace(/ /g, "")
+	var unique = {};
+  for (var i = 0; i < string2.length; i++) {
+  	if (unique[string2[i]] == undefined) {
+      unique[string2[i]] = 1;
+  	} else {
+  		unique[string2[i]]++
+  	}
+  }
+  return unique;
 }
+
+console.log(countLetters("lighthouse in the house"))
